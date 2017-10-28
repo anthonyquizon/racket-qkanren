d11f4bd61340124a4d6d2d0b399025451d4686b5
Anthony Quizon <anthoq88@gmail.com>
2017-10-28 21:00:37 +1100

    fixed: syntax defines

--- src/impl/syntax.rkt
+++ src/impl/syntax.rkt
@@ -13,24 +13,18 @@
   fresh
   run)
 
-(define-syntax Zzz
-  (syntax-rules []
-    [(_ g) (lambda (s/c) 
-             (lambda [] (g s/c)))]))
-
 (define-syntax conj+
   (syntax-rules []
-    [(_ g) (Zzz g)]
-    [(_ g0 g ...) (b:conj (Zzz g0) (conj+ g ...))]))
+    [(_ g) g]
+    [(_ g0 g ...) (b:conj g0 (conj+ g ...))]))
 
 (define-syntax disj+
   (syntax-rules []
-    [(_ g) (Zzz g)]
-    [(_ g0 g ...) (b:disj (Zzz g0) (disj+ g ...))])) 
+    [(_ g) g]
+    [(_ g0 g ...) (b:disj g0 (disj+ g ...))])) 
 
-(define-syntax conde
-  (syntax-rules []
-    [(_ (g0 g ...) ...) (disj+ (conj+ g0 g ...) ...)]))
+(define-syntax-rule (conde (g0 g ...) (g0* g* ...) ...)
+  (disj+ (conj+ g0 g ...) (conj+ g0* g* ...)  ...))
 
 (define-syntax fresh
   (syntax-rules []
@@ -39,7 +33,6 @@
      (b:call/fresh (lambda [x0] (fresh [x ...] g0 g ...)))]))
 
 (define-syntax-rule (run n/b (q) g g* ...)
-  (call/initial-state n/b (fresh (q) g g* ...)))
+  (h:take n/b (h:pull 
+                ((fresh (q) g g* ...) `(,c:S0 . 0)))))
 
-(define (call/initial-state n g) 
-  (h:take n (h:pull (g `(,c:S0 . 0)))))
